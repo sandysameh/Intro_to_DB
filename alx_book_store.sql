fa48861cc252ed253f51c3b1390e3ed272f7f24f
sandysameh2 <sandy.sameh@nodogoro.com>
2024-08-11 19:55:06 +0300

    edit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,8 +27,7 @@ CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
-
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 
 CREATE TABLE Order_Details (
